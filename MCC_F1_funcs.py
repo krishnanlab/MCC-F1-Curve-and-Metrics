dbd700a3e849447d34926a0cbb43e17e9ee5dd28
Vignesh Sridhar <59941977+Vigs23@users.noreply.github.com>
2020-10-26 18:27:52 -0400

    Add files via upload

--- MCC_F1_funcs.py
+++ MCC_F1_funcs.py
@@ -1,6 +1,6 @@
 import sklearn.metrics
-import numpy 
-import matplotlib.pyplot 
+import numpy as np
+import matplotlib.pyplot as plt 
 # MCC-F1 Funtion:
 def mcc_f1(ground_truths, predicted_values):
     
@@ -23,28 +23,28 @@ def mcc_f1(ground_truths, predicted_values):
     mcc = []   
     f1 = []
     
-# Generating thresholds with the PRC function
+    # Generating thresholds with the PRC function
     precision, recall, thresholds = sklearn.metrics.precision_recall_curve(
                                     ground_truths, predicted_values)
 
-# For every value of the thresholds (cutoffs), calculate MCC and F1 scores :
-    for thresh in thresholds:
-        y_pred_thresh = predicted_values > thresh  # Positive classes are determined for every predicted value that exceeds the threshold. 
+    # For every value of the thresholds (cutoffs), calculate MCC and F1 scores :
+    for T in thresholds:
+        predicted_values_after_cutoff = predicted_values > T  # Positive classes are determined for every predicted value that exceeds the threshold. 
     
         mcc.append((sklearn.metrics.matthews_corrcoef(
-                    ground_truths, y_pred_thresh)+1)*0.5) # get normalised MCC: change the range of MCC from [-1, 1] to [0, 1] 
+                    ground_truths, predicted_values_after_cutoff)+1)*0.5) # get normalised MCC: change the range of MCC from [-1, 1] to [0, 1] 
         
         f1.append(sklearn.metrics.f1_score(
-                    ground_truths, y_pred_thresh))
+                    ground_truths, predicted_values_after_cutoff))
         
-    mcc = numpy.array(mcc)
-    f1 = numpy.array(f1)  
+    mcc = np.array(mcc)
+    f1 = np.array(f1)  
     
-  # Resulting values are an array of MCC & F1 scores across different thresholds (cutoffs)  
+    # Resulting values are an array of MCC & F1 scores across different thresholds (cutoffs)  
     return mcc,f1, thresholds
 
 # MCC-F1 Metric:
-def MCC_F1_Metric(mcc,f1,thresholds, W=100):
+def MCC_F1_Metric(mcc,f1,thresholds, W_num_of_subranges = 100):
     
     """
     MCC_F1_Metric function computes the MCC-F1 metric i.e. the average distances between the predcition scores to the point of Perfect Performace (1,1) 
@@ -54,9 +54,9 @@ def MCC_F1_Metric(mcc,f1,thresholds, W=100):
     ----------
     
     mcc,f1, thresholds : numpy.ndarray;
-                            MCC and F1 scores across varying thresholds
+                        MCC and F1 scores across varying thresholds
     
-    W : int, optional;
+    W_num_of_subranges : int, optional;
         Number of subranges- default = 100; 
         larger values of 𝑊 will cause the MCC-𝐹1 metric to capture the performance of a classifier more accurately.
     
@@ -66,44 +66,43 @@ def MCC_F1_Metric(mcc,f1,thresholds, W=100):
                 The ratio of the average distance of the MCC-F1 score to the Point of Perfect Performace (1,1)
                 A metric to compare classifier performance.
     
-    T_index : int;
+    Best_threshold : int;
                 Index of the best prediction score threshold (𝑇), 
                 Index of the point on the MCC-𝐹1 curve closest to the point of perfect performance (1,1).
     
     """
     
      
-    mcc_intervals = numpy.linspace(numpy.min(mcc), numpy.max(mcc),W) # breaking into subranges
+    subrange_intervals = np.linspace(np.min(mcc), np.max(mcc),W_num_of_subranges) # breaking into subranges
     
     # Computing MCC values per subrange
     
-    n = numpy.zeros_like(mcc_intervals) # 'n' is the number of points per subrange
-    for i in range(W-1):
+    values_per_subrange = np.zeros_like(subrange_intervals) # 'n' is the number of points per subrange
+    for i in range(W_num_of_subranges-1):
         for j in mcc:
-            if j >= mcc_intervals[i] and j < mcc_intervals[i+1]:
-                n[i] = n[i] + 1
+            if j >= subrange_intervals[i] and j < subrange_intervals[i+1]:
+                values_per_subrange[i] = values_per_subrange[i] + 1
 
     # Calculating the distances between points in a subrange to the Point of perfect performance (1,1)            
-    # Di = numpy.zeros_like(mcc)
-    Di = numpy.sqrt(((mcc-1)**2)+((f1-1)**2)) # calculating the Euclidean distance 𝐷𝑖            
-    sum_Di = numpy.zeros_like(mcc_intervals)
+    Distance_of_points_within_subrange = np.sqrt(((mcc-1)**2)+((f1-1)**2)) # calculating the Euclidean distance 𝐷𝑖            
+    sum_of_distance_within_subrange = np.zeros_like(subrange_intervals)
     index = -1
     for value in mcc:
         index += 1
-        for i in range(W-1):
-                 if value >= mcc_intervals[i] and value < mcc_intervals[i+1]:
-                        sum_Di[i] = sum_Di[i] + Di[index]
+        for i in range(W_num_of_subranges-1):
+                 if value >= subrange_intervals[i] and value < subrange_intervals[i+1]:
+                        sum_of_distance_within_subrange[i] = sum_of_distance_within_subrange[i] + Distance_of_points_within_subrange[index]
 
-# Mean Distance across subranges
-    mean_Di = numpy.array(sum_Di/n)
-    P = 0  
-    mean_Di_sum = 0
-    for i in mean_Di:
-        if not numpy.isnan(i):
-            P += 1 
-            mean_Di_sum += i # addition of all the means across subranges that have atleast 1 MCC value.
+    # Mean Distance across subranges
+    mean_Distance_per_subrange = np.array(sum_of_distance_within_subrange/values_per_subrange)
+    total_number_of_subranges = 0  
+    total_sum_of_mean_distances = 0
+    for i in mean_Distance_per_subrange:
+        if not np.isnan(i):
+            total_number_of_subranges += 1 
+            total_sum_of_mean_distances += i # addition of all the means across subranges that have atleast 1 MCC value.
 
-    grand_avg = mean_Di_sum/P # P = total number of subranges that have atleast 1 MCC value
+    grand_mean_distance = total_sum_of_mean_distances/total_number_of_subranges # P = total number of subranges that have atleast 1 MCC value
    
     """ 
     Compare the grand average distance to √2 (The distance between the point of worst performance (0,0) and 
@@ -112,31 +111,31 @@ def MCC_F1_Metric(mcc,f1,thresholds, W=100):
     This ratio ranges between 0 and 1 (worst value = 0; best value = 1). To get the MCC-𝐹1 score, we subtract this ratio from 1
     """
 
-    MCC_F1_Met = 1 - (grand_avg /numpy.sqrt(2))
+    MCC_F1_Met = 1 - (grand_mean_distance /np.sqrt(2))
   
     
     # Finding the best threshold 𝑇 the point on the MCC-𝐹1 curve closest to the point of perfect performance (1,1).
    
-    result = numpy.where(Di == Di.min())
-    T_index = result[0][0]
+    result = np.where(Distance_of_points_within_subrange == Distance_of_points_within_subrange.min())
+    Best_threshold = result[0][0]
     
-    print("MCC-F1 metric = {0:.2f}".format(MCC_F1_Met), "Best Threshold = {0:.2f}".format(thresholds[T_index]))
+    print("MCC-F1 metric = {0:.2f}".format(MCC_F1_Met), "Best Threshold = {0:.2f}".format(thresholds[Best_threshold]))
     
-    return MCC_F1_Met,T_index
+    return MCC_F1_Met,Best_threshold
 
 # Generating a plot (the overall canvas);
 axis = None
 def axis_plotting(total_plots): 
     global axis
     if axis == None:
-        fig = matplotlib.pyplot.figure(figsize=(7,7))
+        fig = plt.figure(figsize=(7,7))
         axis = []        
         for i in range(1,total_plots+1):
             ax = fig.add_subplot(total_plots, 1, i)
             axis.append(ax)
             
     
-def plotting(mcc, f1, thresholds,MCC_F1_Met,T_index,classifer_name = "",total_plots = 1,ax = 0):
+def plotting(mcc, f1, thresholds,MCC_F1_Met,Best_threshold,classifer_name = "",total_plots = 1,ax = 0):
     
         """
         Plotting function - fuction to plot the MCC and F1 scores (across varying Thresholds), and highlight the the MCC_F1 metric with the best threshold
@@ -151,7 +150,7 @@ def plotting(mcc, f1, thresholds,MCC_F1_Met,T_index,classifer_name = "",total_pl
                 The ratio of the average distance of the MCC-F1 score to the Point of Perfect Performace (1,1)
                 A metric to compare classifier performance.
     
-        T_index : int;
+        Best_threshold : int;
                 Index of the best prediction score threshold (𝑇), 
                 Index of the point on the MCC-𝐹1 curve closest to the point of perfect performance (1,1).
                 
@@ -174,31 +173,28 @@ def plotting(mcc, f1, thresholds,MCC_F1_Met,T_index,classifer_name = "",total_pl
         """
         
         global axis
-        if total_plots > 1 and ax < total_plots:
-            axis_plotting(total_plots) #calling fucntion to
+        if total_plots > 1 and ax < total_plots: # if more than 1 total plot is mentioned;
+            axis_plotting(total_plots) #calling function to generate plot;
             if ax == 0:
-                subplot = matplotlib.pyplot.gca()
+                subplot = plt.gca() # plot on current axis value, 
             else:
-                subplot = axis[ax-1]
+                subplot = axis[ax-1] # plotting additional graphs based on index of the axis.
         elif ax > total_plots:
             print("Index inconsistent with total_plots")
         else:
             axis = None
-            subplot = matplotlib.pyplot.gca()
-        
-        
-        
+            subplot = plt.gca() # plot on base plot if no additional subplots are needed
       
         # Labelling the plot;
         subplot.plot(f1,mcc)
         subplot.annotate(classifer_name + "_MCC_Met - {0:.2f}".format(
             MCC_F1_Met), 
-                 (f1[T_index],mcc[T_index]), 
+                 (f1[Best_threshold],mcc[Best_threshold]), 
                  textcoords="offset points", 
                  xytext=(0,10),
                  ha='left') 
         
-        subplot.scatter(f1[T_index],mcc[T_index],color='red')
+        subplot.scatter(f1[Best_threshold],mcc[Best_threshold],color='red')
         subplot.set(xlabel='F1 score',
                ylabel='unit−normalized MCC',title='MCC-F1 curve')
         
@@ -217,6 +213,6 @@ def plotting(mcc, f1, thresholds,MCC_F1_Met,T_index,classifer_name = "",total_pl
         subplot.annotate('Point of Worst performance',(0,0)
                     ,textcoords="offset points",xytext=(9,2),ha='left')
         
-        subplot.set_xticks(numpy.arange(0, 1.1, 0.25)); subplot.set_yticks(numpy.arange(0, 1.1, 0.25))
+        subplot.set_xticks(np.arange(0, 1.1, 0.25)); subplot.set_yticks(np.arange(0, 1.1, 0.25))
         subplot.set_xlim(-0.05,1.05); subplot.set_ylim(-0.05,1.05)
         subplot.grid(alpha = 0.3)
